8644d88ee5e21a75a71fc35ae9e6135e256f9014
ReiszD <reiszdavis@gmail.com>
2025-10-17 11:43:36 -0400

    Firebase Config Console.log added

--- firebase.js
+++ firebase.js
@@ -21,6 +21,15 @@ import {
   getDoc,
 } from "firebase/firestore";
 
+console.log("🔥 ENV CHECK:", {
+  API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  MSG_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+});
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -29,7 +38,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
+console.log("🚀 Firebase Config at build:", firebaseConfig);
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
